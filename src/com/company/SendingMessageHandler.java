2e69587d284b7c79349728fe8ce92fed06f54d76
Dushan Kumarasinghe <dushan.14@cse.mrt.ac.lk>
2018-09-20 22:27:02 +0530

    file search msg handling implemented

--- src/com/company/SendingMessageHandler.java
+++ src/com/company/SendingMessageHandler.java
@@ -1,20 +1,22 @@
 package com.company;
 
 import java.util.Map.Entry;
+import java.util.StringTokenizer;
+
 import static com.company.Client.*;
 import static com.company.Util.*;
 
 public class SendingMessageHandler {
 
     /**
-     * command: reg 127.0.0.1
+     * command: reg bs_ip_address
      *
      * @param bs_ip
      */
     public static void registerToBS(String bs_ip) {
 
         /* length REG IP_address port_no username */
-        String msg="REG "+myIp+" "+myPort+" "+myUserName;
+        String msg="REG " + myIp + " " + myPort + " " + myUserName;
         String msg_formatted = formatMessage(msg);
 
         Client.bs_ip=bs_ip;
@@ -28,11 +30,12 @@ public class SendingMessageHandler {
     public static void unregisterFromBS(){
 
         /* length UNREG IP_address port_no username */
-        String msg="UNREG "+myIp+" "+myPort+" "+myUserName;
+        String msg="UNREG " + myIp + " " + myPort + " " + myUserName;
         String msg_formated = formatMessage(msg);
 
         sendPacket(bs_ip,bs_port,msg_formated);
     }
+
 
     /**
      *  command: join
@@ -40,7 +43,7 @@ public class SendingMessageHandler {
     public static void joinToSystem() {
 
         /* length JOIN IP_address port_no */
-        String msg="JOIN "+myIp+" "+myPort;
+        String msg="JOIN " + myIp + " " + myPort;
         String msg_formated = formatMessage(msg);
 
         if (routingTable.size()==0){
@@ -53,19 +56,37 @@ public class SendingMessageHandler {
         }
     }
 
+
     /**
      *  command: leave
      */
     public static void leaveTheSystem() {
 
         /* length LEAVE IP_address port_no */
-        String msg="LEAVE "+myIp+" "+myPort;
+        String msg="LEAVE " + myIp + " " + myPort;
         String msg_formated = formatMessage(msg);
 
         for (Entry<String,Node> entry: routingTable.entrySet()) {
             sendPacket(entry.getValue().getIp(),entry.getValue().getPort(),msg_formated);
         }
 
+    }
+
+
+    /**
+     *  command: search file_name
+     *
+     * @param filename
+     */
+    public static void searchFile(String filename) {
+
+        /* length SER IP port file_name hops */
+        String msg="SER " + myIp + " " + myPort + " " + filename + " " + "hops";
+        String msg_formated = formatMessage(msg);
+
+        for (Entry<String,Node> entry: routingTable.entrySet()) {
+            sendPacket(entry.getValue().getIp(),entry.getValue().getPort(),msg_formated);
+        }
     }
 
 
