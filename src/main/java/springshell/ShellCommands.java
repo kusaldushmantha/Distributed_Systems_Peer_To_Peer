c39847db7122e01d53979c7a9293058f9d31d2ad
Dushan Kumarasinghe <dushan.14@cse.mrt.ac.lk>
2018-10-14 22:26:13 +0530

    commans args set

--- src/main/java/springshell/ShellCommands.java
+++ src/main/java/springshell/ShellCommands.java
@@ -6,16 +6,14 @@ import org.springframework.shell.core.annotation.CliOption;
 import org.springframework.stereotype.Component;
 import udpclient.SendingMessageHandler;
 
-import static udpclient.Printer.printHelp;
-import static udpclient.Printer.printRoutingTable;
-import static udpclient.Printer.printSelectedFiles;
+import static udpclient.Printer.*;
 import static udpclient.Util.changeMyPort;
 import static udpclient.Util.getHelpText;
 
 @Component
 public class ShellCommands implements CommandMarker {
 
-    @CliCommand(value = "reg", help = "[reg server_ip] register to Bootstrap server")
+    @CliCommand(value = "reg", help = "[reg --ip server_ip] register to Bootstrap server")
     public void register(@CliOption(key = "ip") String ip){
         SendingMessageHandler.registerToBS(ip);
     }
@@ -45,8 +43,16 @@ public class ShellCommands implements CommandMarker {
         SendingMessageHandler.leaveTheSystem();
     }
 
-    @CliCommand(value = "search",help = "[search file_name hops(optional)] search files in network by name")
-    public void search(@CliOption(key = "name") String name,@CliOption(key = "hops") int hops){
+    @CliCommand(value = "search",help = "[search --n file_name --h hops(optional)] search files in network by name")
+    public void search(@CliOption(key = {"name" ,"n"}) String name,@CliOption(key = {"hops", "h"}) String hopsStr ){
+        int hops=1;
+        if (hopsStr!=null){
+            try{
+                hops=Integer.parseInt(hopsStr);
+            }catch (Exception e){
+                print_ng("Number format error for hops");
+            }
+        }
         SendingMessageHandler.searchFile(name,hops);
     }
 
@@ -61,7 +67,7 @@ public class ShellCommands implements CommandMarker {
     }
 
     @CliCommand(value = "setport", help = "[setport port] change port if registration failed")
-    public void setport(@CliOption(key = "port") String port){
+    public void setport(@CliOption(key = {"port","p"}) String port){
         changeMyPort(port);
     }
 
